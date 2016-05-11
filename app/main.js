b17d2b06394c7ff70a502c7a933ee8a3d2cde08a
Jaume Sala Tantinya <jaumesalatantinya@gmail.com>
2016-05-11 11:19:37 +0100

    Remove onload

--- app/main.js
+++ app/main.js
@@ -1,7 +1,4 @@
 import Accordion from './components/Accordion';
 
-window.onload = () => {
-    const accordion1 = new Accordion('#a1', 3);
-    accordion1.init();
-    //accordion1.loadAjaxContent('data/users.json');
-};
+const accordion1 = new Accordion('#a1', 3);
+accordion1.init();
